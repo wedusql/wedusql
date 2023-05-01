1173fc0885ee39e4d279f39abf4d8a9f3f89f8c8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-05-01 17:44:09 +0800

    feat(home): show functions and procedures

--- frontend/src/components/BSidebar.vue
+++ frontend/src/components/BSidebar.vue
@@ -15,6 +15,7 @@ const emits = defineEmits(['update:activeTable']);
 const getAllTables = async () => {
   const qTables = await Run("SHOW TABLES");
   tables.value = [
+    ...tables.value,
     {
       key: "0",
       label: "Tables",
@@ -28,8 +29,45 @@ const getAllTables = async () => {
   ];
 };
 
+const getAllProcedures = async () => {
+  const qProcedures = await Run("SHOW PROCEDURE STATUS WHERE db = DATABASE() AND type = 'PROCEDURE'");
+  tables.value = [
+    ...tables.value,
+    {
+      key: "1",
+      label: "Procedures",
+      children: qProcedures.Rows.map((t: any) => {
+        return {
+          key: `procedures-${t.Name}`,
+          label: t.Name,
+        };
+      }),
+    },
+  ];
+};
+
+const getAllFunctions = async () => {
+  const qFunctions = await Run("SHOW FUNCTION STATUS WHERE db = DATABASE() AND type = 'FUNCTION'");
+  tables.value = [
+    ...tables.value,
+    {
+      key: "2",
+      label: "Functions",
+      children: qFunctions.Rows.map((t: any) => {
+        return {
+          key: `functions-${t.Name}`,
+          label: t.Name,
+        };
+      }),
+    },
+  ];
+};
+
+
 onMounted(() => {
   getAllTables();
+  getAllProcedures();
+  getAllFunctions();
 });
 
 watchEffect(() => {
